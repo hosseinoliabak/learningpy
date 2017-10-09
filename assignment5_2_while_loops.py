9779fea26e66ea880c0dcbdb1e7ec9e632c5e1df
ehosoli <h.oliabak@outlook.com>
2017-10-09 09:50:22 -0400

    add assignment5_2_while_loops

--- assignment5_2_while_loops.py
+++ assignment5_2_while_loops.py
@@ -9,3 +9,20 @@ smallest of the numbers. If the user enters anything other than a valid number
 catch it with a try/except and put out an appropriate message and ignore the
 number. Enter 7, 2, bob, 10, and 4 and match the output below.
 '''
+iLargest = None
+iSmallest =None
+while True:
+    sInput = input('Enter and Integer or stop entering by typing "done": ')
+    if str.lower(sInput) == 'done' : break
+    try:
+        iInput = int(sInput)
+    except:
+        print('Invalid input')
+    else:
+        if iLargest is None or iInput > iLargest:
+            iLargest = iInput
+        if iSmallest is None or iInput < iSmallest:
+            iSmallest = iInput
+
+print ("Maximum is", iLargest)
+print ("Minimum is", iSmallest)
