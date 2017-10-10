835d1c81a64a4164b1d405237c85dff7e0ec286e
ehosoli <h.oliabak@outlook.com>
2017-10-10 11:10:27 -0400

    add assignment8_5_list

--- assignment8_5_list.py
+++ assignment8_5_list.py
@@ -8,4 +8,51 @@ out a count at the end.
 Hint: make sure not to include the lines that start with 'From:'.
 
 You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+Desired Output:
+stephen.marquard@uct.ac.za
+louis@media.berkeley.edu
+zqian@umich.edu
+rjlowe@iupui.edu
+zqian@umich.edu
+rjlowe@iupui.edu
+cwen@iupui.edu
+cwen@iupui.edu
+gsilver@umich.edu
+gsilver@umich.edu
+zqian@umich.edu
+gsilver@umich.edu
+wagnermr@iupui.edu
+zqian@umich.edu
+antranig@caret.cam.ac.uk
+gopal.ramasammycook@gmail.com
+david.horwitz@uct.ac.za
+david.horwitz@uct.ac.za
+david.horwitz@uct.ac.za
+david.horwitz@uct.ac.za
+stephen.marquard@uct.ac.za
+louis@media.berkeley.edu
+louis@media.berkeley.edu
+ray@media.berkeley.edu
+cwen@iupui.edu
+cwen@iupui.edu
+cwen@iupui.edu
+There were 27 lines in the file with From as the first word
 '''
+lFromsList = list()
+lWordsList = list() # this is the same as lWordsList = list[]
+iCount = 0
+try:
+    fiHandle = open("mbox-short.txt")
+except:
+    print('There is no "mbox-short.txt" file in the same folder as this script.')
+else:
+    for sLines in fiHandle:
+        if not sLines.startswith('From '):
+            continue
+        lFromsList.append(sLines)
+    for sWords in lFromsList:
+        iCount += 1
+        lWrodsList = sWords.split()
+        print(lWrodsList[1])
+    print('There were',iCount,'lines in the file with From as the first word')
+    fiHandle.close()
