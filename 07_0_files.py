f1344777ed29cccd5b532b0bb76225a2d4066622
ehosoli <h.oliabak@outlook.com>
2017-10-17 11:27:10 -0400

    Edit 07_0

--- 07_0_files.py
+++ 07_0_files.py
@@ -30,4 +30,22 @@ for line in flHand:
     if not line.startswith('From:') :
         continue
     print(line)
+
+
+
+Main file processing modes:
+
+'r' Open for reading (Default). The file pointer is placed at the beginning of
+    the file.
+
+'w' Open for writing. Overwrites the file if the file exists. If the file does
+    not exist, creates a new file for writing.
+
+'a' Open for appending to the end of the file. The file pointer is at the end
+    of the file if the file exists. That is, the file is in the append mode. If
+    the file does not exist, it creates a new file for writing.
+
+'b' Binary Mode
+
+'+' ('r+', 'w+') Open a file for reading and writing
 '''
