15599bf13a910ce154588a16deb542dc96b1fe52
ehosoli <h.oliabak@outlook.com>
2017-10-22 18:43:22 -0400

    Work on OOP

--- 14_1_oop.py
+++ 14_1_oop.py
@@ -11,8 +11,8 @@ Create a class “Vehicle” (Exploring simple classes)
 . Create a method called set_speed(speed) that sets the speed of the vehicle.
 
 Once your Class is done, use it to create the following objects:
-V01 = vehicle(4,2,100)
-V02 = vehicle(6,4,75)
+V01 = Vehicle(4, 2, 100)
+V02 = Vehicle(6, 4, 75, 30)
 
 Print the current_speed of both vehicles
 Set the speed of vehicle V01 to 90 Set the speed of vehicle V02 to 60
@@ -22,13 +22,25 @@ Print the value vehicle_terrain for both vehicles
 class Vehicle:
     vehicle_terrain = "land"
 
-    def __ini__(self, iNumWheels, iNumPassengers, iMaxSpeed, itSpeed = 0):
+    def __init__(self, iNumWheels, iNumPassengers, iMaxSpeed, iSpeed = 0):
         self.numWheels = iNumWheels
         self.numPassengers = iNumPassengers
         self.maxSpeed = iMaxSpeed
         self.speed = iSpeed
 
+    @property
     def current_speed(self):
         return self.speed
 
-    def current_speed()
+    @current_speed.setter
+    def current_speed(self, iCurrentSpeed):
+        self.speed = iCurrentSpeed
+
+V01 = Vehicle(4, 2, 100)
+V02 = Vehicle(6, 4, 75, 30)
+
+print(V01.current_speed)
+print(V02.current_speed)
+V01.current_speed = 95
+V02.current_speed = 60
+print('V1\'s speed is {} and V2\'s speed is {}'.format(V01.current_speed, V02.current_speed))
