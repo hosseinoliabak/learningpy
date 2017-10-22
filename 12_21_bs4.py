6cdad475f5a946a36383b9ab391714f431fd6aa2
ehosoli <h.oliabak@outlook.com>
2017-10-22 17:11:46 -0400

    Categorize chapter 12

---
+++ 12_21_bs4.py
@@ -0,0 +1,26 @@
+'''
+Using beautifulsoup write a program that does the following:
+Open the URL www.meteomedia.com
+Find how many <a> tags are there in the html code
+Print all href links
+'''
+# The variables start with:
+# s -> string; i -> integer; f -> float; b -> boolean; fl -> file;
+# t -> tuples; l -> list; d -> dictionary; ; byte -> bytes;
+# sock -> socket object; bs -> BeautifulSoup object
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+byteHtml = urllib.request.urlopen('http://www.httpbin.org').read()
+bsSoup = BeautifulSoup(byteHtml, "html.parser")
+bsElementResultSet = bsSoup('a')
+
+for bsElementTag in bsElementResultSet:
+    sHref = bsElementTag.get('href', None)
+    lHrefContents = bsElementTag.contents
+    dAttrs = bsElementTag.attrs
+    print('TAG:', bsElementTag)
+    print('URL:', sHref)
+    print('Contents:', lHrefContents[0])
+    print('Attrs:', dAttrs)
