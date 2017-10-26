986d2a87efe9967a39b84a63912c54d6b42d5b98
ehosoli <h.oliabak@outlook.com>
2017-10-25 20:34:09 -0400

    Work on CSV

--- 13_21_csv.py
+++ 13_21_csv.py
@@ -4,14 +4,14 @@ Read the content of sample-csv.csv then produce sample-email.csv wherein
 changed to 'tab' (\t). After running the script The content of sample-email.csv
 should be this:
 
-+-----------------------------------+
++--------------------------------------+
 ﻿User Name	Display Name
 chris@contoso.com	Chris Green
 ben@contoso.com	Ben Andrews
 david@contoso.com	David Longmuir
 cynthia@contoso.com	Cynthia Carey
 melissa@contoso.com	Melissa MacBeth
-+-----------------------------------+
++--------------------------------------+
 
 For this assessment you must use DictReader(), DictWriter(). Other functions
 you need to use are: writehedear(), and writerow()
