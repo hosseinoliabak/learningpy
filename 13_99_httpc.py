b72df5bd1dd1f43172b7dea33f19a79f51e0cbb5
ehosoli <h.oliabak@outlook.com>
2017-10-27 04:44:15 -0400

    Rename the project to httpc

--- 13_99_httpc.py
+++ 13_99_httpc.py
@@ -325,3 +325,42 @@ Comp6461 – Fall 2017 - Lab Assignment # 1 Page 11
 [4] cURL: https://curl.haxx.se.
 [5] Telnet: https://en.wikipedia.org/wiki/Telnet.
 '''
+import argparse
+
+parser = argparse.ArgumentParser(description='cURL-like Command Line Implementation')
+parser.add_argument('-x', '--length', type=float, metavar='', required=True, help='Length of the rectangle/cube')
+parser.add_argument('-y', '--width', type=float, metavar='', required=True, help='Width of the rectangle/cube')
+parser.add_argument('-z', '--height', type=float, metavar='', help='Height of the rectangle/cube')
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-q', '--quiet', action='store_true', help='print quiet')
+group.add_argument('-v', '--verbose', action='store_true', help='print verbose')
+args = parser.parse_args()
+
+def area(length, width):
+    area = length * width
+    resultString = "Area of the rectangle"
+    return area, resultString
+
+def volume(length, width, height):
+    vol = length * width * height
+    resultString = "Volume of the cube"
+    return vol, resultString
+
+if __name__ == '__main__':
+    if not args.height:
+        result = area(args.length, args.width)[0]
+        resultString = area(args.length, args.width)[1]
+        resultVerbose = resultString + ' with length {} and width {} is:'.format(args.length, args.width)
+    else:
+        result = volume(args.length, args.width, args.height)[0]
+        resultString = volume(args.length, args.width, args.height)[1]
+        resultVerbose = resultString + ' with length {}, width {}, and height {} is:'.format(args.length, args.width, args.height)
+
+    if args.quiet:
+        print (result)
+
+    elif args.verbose:
+        print(resultVerbose, result)
+    else:
+        print(resultString, result)
