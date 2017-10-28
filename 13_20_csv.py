894f4bf29780ee58896637938eef3679677be406
ehosoli <h.oliabak@outlook.com>
2017-10-28 14:45:49 -0400

    Work on csv

--- 13_20_csv.py
+++ 13_20_csv.py
@@ -9,9 +9,13 @@ with open('sample-csv.csv') as csv_file:
 Another way to read from a csv file is using DictReader() instead of reader()
 Read the sample-csv.csv file taking advantage of DictReader()
 '''
+# The variables start with:
+# s -> string; i -> integer; f -> float; b -> boolean; fl -> file;
+# t -> tuples; l -> list; d -> dictionary; od -> ordered dictionary;
+# byte -> bytes; sock -> socket object; bs -> BeautifulSoup object
 import csv
 with open('sample-csv.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
-    for line in csv_reader:
-        print(line)
+    for odLine in csv_reader:
+        print(odLine)
