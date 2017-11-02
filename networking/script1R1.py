9a2183ade913be5d4ac8af48e86ef03ba41e9645
hosseinoliabak <h.oliabak@outlook.com>
2017-11-02 13:38:22 -0400

    Work on script1R1

---
+++ networking/script1R1.py
@@ -0,0 +1,19 @@
+import getpass
+import telnetlib
+
+HOST = "10.10.32.3"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"en\n")
+
+print(tn.read_all().decode('ascii'))
+
