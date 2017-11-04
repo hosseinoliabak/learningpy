483d5a9883721a95592fc792344c6a26ed35ffcf
hosseinoliabak <h.oliabak@outlook.com>
2017-11-04 15:48:35 -0400

    Backup from Switches configuration

---
+++ networking/01_6_backup_sw_config.py
@@ -0,0 +1,29 @@
+import getpass
+import telnetlib
+
+def telnettocisco(ip, user, password):
+
+    tn = telnetlib.Telnet(ip)
+
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+
+    tn.write(b"terminal length 0\n")
+
+    tn.write(b"show run\n")
+    tn.write(b"exit\n")
+    return tn.read_all().decode('ascii')
+
+
+username = input("Enter your telnet username: ")
+passwd = getpass.getpass()
+
+with open("01_5_Switches_IPs.txt", "r") as flSwitches:
+    for line in flSwitches:
+        line = line.strip()
+        with open("01_Switch"+ line+ ".conf", "w") as saveOutput:
+            saveOutput.write(telnettocisco(line, username, passwd))
+print("Done!")
