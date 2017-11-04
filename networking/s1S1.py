e39048f0ca8abadf93d78c8cadf44c1efa485af5
hosseinoliabak <h.oliabak@outlook.com>
2017-11-03 20:09:34 -0400

    Work on networking scripts

--- networking/s1S1.py
+++ networking/s1S1.py
@@ -1,7 +1,7 @@
 import getpass
 import telnetlib
 
-HOST = "10.10.32.3"
+HOST = "10.10.32.2"
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
@@ -13,15 +13,15 @@ if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
 
+tn.write(b"enable\n")
+tn.write(b"test\n")
 tn.write(b"conf t\n")
-tn.write(b"int l0\n")
-tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
-tn.write(b"int l1\n")
-tn.write(b"ip address 2.2.2.2 255.255.255.255\n")
-tn.write(b"router ospf 1\n")
-tn.write(b"network 0.0.0.0 255.255.255.255 area 0\n")
+tn.write(b"vlan 2\n")
+tn.write(b"vlan 3\n")
+tn.write(b"vlan 4\n")
+tn.write(b"vlan 5\n")
+tn.write(b"vlan 6\n")
 tn.write(b"end\n")
 tn.write(b"exit\n") # this is essential for the next line not to wait for EOL
 
 print(tn.read_all().decode('ascii'))
-
