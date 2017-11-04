7b7f85a3b65343f26df76535c296b99362f06dd1
hosseinoliabak <h.oliabak@outlook.com>
2017-11-03 22:10:49 -0400

    Telnet to multiple switches and create 100 VLANs

--- networking/01_S1_2.py
+++ networking/01_S1_2.py
@@ -1,30 +1,35 @@
 import getpass
 import telnetlib
 
-HOST = "10.10.32.2"
-user = input("Enter your telnet username: ")
-password = getpass.getpass()
+def telnettocisco(ip, user, password):
 
-tn = telnetlib.Telnet(HOST)
+    tn = telnetlib.Telnet(ip)
 
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
-if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
 
-tn.write(b"enable\n")
-tn.write(b"test\n")
-tn.write(b"conf t\n")
+    tn.write(b"enable\n")
+    tn.write(b"test\n")
+    tn.write(b"conf t\n")
 
 
-
-for i in range(2, 101):
-    tn.write(b"vlan "+ str(i).encode('ascii')+ b"\n")
-    tn.write(b"name Python_VLAN_" + str(i).encode('ascii') + b"\n")
-
+    for i in range(2, 101):
+        tn.write(b"vlan "+ str(i).encode('ascii')+ b"\n")
+        tn.write(b"name Python_VLAN_" + str(i).encode('ascii') + b"\n")
 
 
-tn.write(b"end\n")
-tn.write(b"exit\n") # this is essential for the next line not to wait for EOL
-print(tn.read_all().decode('ascii'))
+    tn.write(b"end\n")
+    tn.write(b"exit\n") # this is essential for the next line not to wait for EOL
+    print(tn.read_all().decode('ascii'))
+
+
+
+username = input("Enter your telnet username: ")
+passwd = getpass.getpass()
+
+HOST = [("10.10.22." + str(n)) for n in range(11, 15)]
+for n in HOST:
+    telnettocisco(n, username, passwd)
