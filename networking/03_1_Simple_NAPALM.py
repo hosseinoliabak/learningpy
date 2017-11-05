26238b1dbbb6fa47b2dc11322f0448096b830876
hosseinoliabak <h.oliabak@outlook.com>
2017-11-05 12:29:32 -0500

    NAPALM getters

--- networking/03_1_Simple_NAPALM.py
+++ networking/03_1_Simple_NAPALM.py
@@ -24,12 +24,27 @@ get_interfaces()
 '''
 from napalm import get_network_driver
 import json
+
+
 driver = get_network_driver('ios')
 sw1 = driver('192.168.122.10', 'test', 'test')
 sw1.open()
 
+# See Getters support matrix:
+# https://napalm.readthedocs.io/en/latest/support/index.html
 sw_output = sw1.get_facts()
 print(json.dumps(sw_output, sort_keys=True, indent=4))
 
-if_output = sw1.get_interfaces()
-print(json.dumps(if_output, sort_keys=True, indent=4))
+sw_output = sw1.get_interfaces()
+print(json.dumps(sw_output, sort_keys=True, indent=4))
+
+sw_output = sw1.get_mac_address_table()
+print(json.dumps(sw_output, sort_keys=True, indent=4))
+
+
+sw_output = sw1.get_arp_table()
+print(json.dumps(sw_output, sort_keys=True, indent=4))
+
+
+sw_output = sw1.ping('192.168.122.1')
+print(json.dumps(sw_output, sort_keys=True, indent=4))
