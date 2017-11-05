100b851a68b81dba0aa950343331c7947140914b
hosseinoliabak <h.oliabak@outlook.com>
2017-11-05 13:15:09 -0500

    Get BGP neighbors of devices using NAPALM

--- networking/03_2_NAPALM-BGP.py
+++ networking/03_2_NAPALM-BGP.py
@@ -1,13 +1,17 @@
 from napalm import get_network_driver
 import json
 
-driver = get_network_driver('ios')
-bgplist= ['192.168.122.10', '192.168.122.30']
-
-for ip in bgplist:
+def print_bgp_neighbors(ip):
     driver = get_network_driver('ios')
     iosv = driver(ip, 'test', 'test')
     iosv.open()
     bgp_neighbors = iosv.get_bgp_neighbors()
     print(json.dumps(bgp_neighbors, sort_keys=True, indent=4))
     iosv.close()
+
+
+bgplist= ['192.168.122.10', '192.168.122.30']
+
+for ip in bgplist:
+    print("Gathering the BGP neighbor of: "+ ip)
+    print_bgp_neighbors(ip)
