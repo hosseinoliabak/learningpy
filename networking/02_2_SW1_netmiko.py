410c2f2818ac6839c2b2da7b187c8f8a2942bf5b
hosseinoliabak <h.oliabak@outlook.com>
2017-11-05 09:36:42 -0500

    Work on Netmiko

---
+++ networking/02_2_SW1_netmiko.py
@@ -0,0 +1,27 @@
+from netmiko import ConnectHandler, cisco
+
+mydevice = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.10',
+    'username': 'test',
+    'password': 'test',
+    'verbose': True
+}
+
+ssh_conn = ConnectHandler(**mydevice)
+print("\n\n")
+
+print(ssh_conn.send_command('sho ip int br'))
+
+command_list = ['int l3', 'ip address 3.3.3.3 255.255.255.255']
+output = ssh_conn.send_config_set(command_list)
+print(output)
+
+
+for i in range (2, 21):
+    command_list = ['vlan '+ str(i), 'name VLAN'+ str(i)]
+    output = ssh_conn.send_config_set(command_list)
+    print(output)
+
+
+ssh_conn.send_command('wr')
