ceadf3b191ff880a91cbdd4d866bfa04fbea3788
hosseinoliabak <h.oliabak@outlook.com>
2017-11-06 08:25:12 -0500

    Check if ACL already exists and add if missing

---
+++ networking/03_3_NAPALM_Config_ACL_if_exists.py
@@ -0,0 +1,17 @@
+from napalm import get_network_driver
+
+driver = get_network_driver('ios')
+sw1 = driver('192.168.122.10', 'test', 'test')
+sw1.open()
+
+print('Accessing 192.168.122.10...')
+sw1.load_merge_candidate(filename='ACL1.cfg')
+
+diffs = sw1.compare_config()
+if len(diffs) > 0:
+    sw1.commit_config()
+else:
+    print('No changes required!')
+    sw1.discard_config()
+
+sw1.close()
