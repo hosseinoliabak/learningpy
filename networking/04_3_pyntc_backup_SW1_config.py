703cc7398e14b82cd2c0727cf002a27f17c9bc16
hosseinoliabak <h.oliabak@outlook.com>
2017-11-07 06:10:32 -0500

    Save backup configuration as a file

---
+++ networking/04_3_pyntc_backup_SW1_config.py
@@ -0,0 +1,13 @@
+from pyntc import ntc_device as NTC
+
+HOST = '192.168.122.10'
+sw1 = NTC(host=HOST, username='test', password='test', device_type="cisco_ios_ssh")
+sw1.open()
+
+sw1_runningConfig = sw1.running_config
+
+        with open("01_Switch"+ line+ ".conf", "w") as saveOutput:
+            print("Saving the configuration of "+ line+ "...")
+            saveOutput.write(telnetToCisco(line, username, passwd))
+            print()
+print("All Done! Browse the current directory to locate the backups!")
